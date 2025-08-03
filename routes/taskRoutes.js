5793a2baba3079b3922d7b482a30493b8c6462a2
DK <don@promptnow.com>
2025-08-03 18:29:54 +0700

    Update taskRoutes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,23 +2,34 @@ const express = require("express");
 const taskService = require("../services/taskService");
 const securityManager = require("../security/securityManager");
 const validator = require("../utils/validator");
-
 const taskRoutes = express.Router();
+
 taskRoutes
-	.route("/")
-	.get(
-		securityManager.verifyToken,
-		taskService.getAllTasks)
-	.post(
-		securityManager.verifyToken,
-		taskService.createTask);
+    .route("/")
+    .get(
+        securityManager.verifyToken,
+        taskService.getAllTasks
+    )
+    .post(
+        securityManager.verifyToken,
+        taskService.createTask
+    );
 taskRoutes
-	.route("/:id")
-	.patch(
-		securityManager.verifyToken,
-		securityManager.verifyAdminLevelClearance,
-		validator.validateId,
-		taskService.updateTaskById
-	);
+    .route("/:id")
+    .get(
+        securityManager.verifyToken,
+        validator.validateId,
+        taskService.getTaskById
+    )
+    .put(
+        securityManager.verifyToken,
+        validator.validateId,
+        taskService.updateTaskById
+    )
+    .delete(
+        securityManager.verifyToken,
+        validator.validateId,
+        taskService.deleteTaskById
+    );
 
 module.exports = taskRoutes;
